803f65d1c6895cdac91a35de479fa9d2802f288c
sunpreetsandhey <147057463+sunpreetsandhey@users.noreply.github.com>
2023-11-09 10:17:57 +0000

    Add files via upload

---
+++ Week-1.py
@@ -0,0 +1,38 @@
+# print("the program has executed")
+# help(print)
+# quit()
+
+
+# Exercises ####
+
+print(45+20)
+
+print(10+20-15)
+
+print(10*5)
+
+print(100/33)
+
+print(100//33)
+
+print(10**2)
+
+print(10%3)
+
+print(10 + 5 * 2)
+
+print(10 - 5 * 10 + 5)
+
+print(5 * 10 ** 2)
+
+print((10 + 5) * 2)
+
+print(10 - 5 * (10 + 5))
+
+print((5 * 10) ** 2)
+
+print(12 + (5 * 2 + 3))
+
+print(12 + (5 * (2 + 3)))
+
+print ("ten divided by zero is",10/0)
